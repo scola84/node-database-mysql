c83315bb98c573283138487fbad06b7e59cce96b
M.C. van den Boogaart <scola@kvdb.name>
2016-02-03 14:06:20 +0100

    Do not manage queries

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ const Database = require('./lib/database.js');
 
 class Module extends DI.Module {
   configure() {
-    this.inject(Database)
-      .insertArgument(1, this.value(mysql));
+    this.inject(Database).with(
+      this.value(mysql)
+    );
   }
 }
 
