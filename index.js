051840a713e74e832764dd279ce94e86687ccef9
M.C. van den Boogaart <scola@kvdb.name>
2016-03-28 23:53:41 +0200

    Add convenience method for serial and parallel read/write

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 'use strict';
 
 const mysql = require('mysql');
+
 const DI = require('@scola/di');
+const Chain = require('@scola/chain');
+
 const Database = require('./lib/database.js');
 
 class Module extends DI.Module {
   configure() {
     this.inject(Database).with(
+      this.object({}),
+      this.instance(Chain),
       this.value(mysql)
     );
   }
